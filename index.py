209d4f6d4192109669b89cdc3c7cb6ebf2a8dcdd
Nikunj Yadav <nikunjy@uber.com>
2017-01-29 00:54:38 -0800

    First commit

---
+++ index.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask_ask import Ask, question, statement
+import requests
+import json
+import unidecode
+app = Flask(__name__)
+ask = Ask(app, "/")
+
+
+def _get_headlines():
+    """
+    Gets the headlines from new york times
+    :return:
+    """
+    url = "http://api.nytimes.com/svc/topstories/v2/world.json?api-key=66fa8330903e437783df1965800be3d7"
+    data = json.loads(requests.get(url).text)
+    headlines = []
+    index = 1
+    for result in data.get("results"):
+        headline = unidecode.unidecode(result.get("title"))
+        headline = "Headline {} : {}".format(index, headline)
+        headlines.append(headline)
+        index += 1
+    headline_string = ""
+    index = 1
+    for headline_d in headlines:
+        headline = unidecode.unidecode(headline_d)
+        index += 1
+        headline_string += headline + ". "
+        if index == 5:
+            break
+    return headline_string
+
+
+@ask.intent("GetNewsIntent")
+def get_headlines():
+    headline_string = _get_headlines()
+    return statement(headline_string)
+
+
+@app.route('/')
+def homepage():
+    return _get_headlines()
+
+
+@ask.launch
+def start_skill():
+  return question("Would you like the top news ?")
+
+if __name__ == '__main__':
+    app.run(debug=True)
